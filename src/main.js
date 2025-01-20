73fe891653692328036a81902efd1dff51247a5f
Nevaeh30 <yingjiahui66@gmail.com>
2025-01-20 13:29:19 +0800

    更新 main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// 导入样式文件
+import './assets/main.css'
+import './assets/base.css'
+// 如果您有其他样式文件，也需要在这里导入
 
 createApp(App).mount('#app')
